4ed7d19a2ed5b80526047aa9ff2ed5067c3ff63b
jack-mcg2007 <jm4044@hw.ac.uk>
2026-04-03 03:19:26 +0100

    added score

--- main.c
+++ main.c
@@ -3,25 +3,30 @@
 float shipX = 300;
 float shipY = 300;
 
+float score = 0;
 
 int main(void)
 {   
     InitWindow(600,600, "Asteroid game");
     while (!WindowShouldClose())
     {   
-        if(IsKeyDown(KEY_A)) shipX -= 0.1;
-        if(IsKeyDown(KEY_D)) shipX += 0.1;
-        if(IsKeyDown(KEY_W)) shipY -= 0.1;
-        if(IsKeyDown(KEY_S)) shipY += 0.1;
-        if(shipX<0) shipX = 0;
-        if(shipX>600) shipX = 600;
+        if(IsKeyDown(KEY_A)) shipX -= 5;
+        if(IsKeyDown(KEY_D)) shipX += 5;
+        if(IsKeyDown(KEY_W)) shipY -= 5;
+        if(IsKeyDown(KEY_S)) shipY += 5;
+        if(shipX<20) shipX = 20;
+        if(shipX>580) shipX = 580;
         if(shipY<0) shipY = 0;
-        if(shipY>600) shipY = 600;
+        if(shipY>570) shipY = 570;
 
+        score +=0.1;
+
+        SetTargetFPS(60);
 
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawTriangle((Vector2){shipX + 20, shipY - 70}, (Vector2){shipX, shipY - 100}, (Vector2){shipX - 20, shipY - 70}, RAYWHITE);
+        DrawTriangle((Vector2){shipX + 20, shipY +15}, (Vector2){shipX, shipY - 15}, (Vector2){shipX - 20, shipY +15}, RAYWHITE);
+        DrawText("Score: 0", 10,10,20,RAYWHITE);
         EndDrawing();
     }
     CloseWindow();
