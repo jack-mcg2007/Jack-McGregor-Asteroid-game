04e5a902064dafe5147b57b954d1ee4ee2582ce7
jack-mcg2007 <jm4044@hw.ac.uk>
2026-04-03 02:58:25 +0100

    created a "ship" which can be moved with arrow keys

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
 #include "raylib.h"
 
+float shipX = 300;
+float shipY = 300;
+
+
 int main(void)
 {   
-    InitWindow(1000,300, "Asteroid game");
+    InitWindow(600,600, "Asteroid game");
     while (!WindowShouldClose())
-    {
+    {   
+        if(IsKeyDown(KEY_A)) shipX -= 0.1;
+        if(IsKeyDown(KEY_D)) shipX += 0.1;
+        if(IsKeyDown(KEY_W)) shipY -= 0.1;
+        if(IsKeyDown(KEY_S)) shipY += 0.1;
+        if(shipX<0) shipX = 0;
+        if(shipX>600) shipX = 600;
+        if(shipY<0) shipY = 0;
+        if(shipY>600) shipY = 600;
+
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
+        DrawTriangle((Vector2){shipX + 20, shipY - 70}, (Vector2){shipX, shipY - 100}, (Vector2){shipX - 20, shipY - 70}, RAYWHITE);
         EndDrawing();
     }
     CloseWindow();
     return 0;
-}   
+}    
