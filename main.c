2b2b8343eede21fac8ff898f1e7ef3e4f705c62a
jack-mcg2007 <jm4044@hw.ac.uk>
2026-04-04 01:27:32 +0100

    finished game

--- main.c
+++ main.c
@@ -3,16 +3,20 @@
 
 float shipX = 300;
 float shipY = 300;
+Texture2D shipTexture;
+//creates variables for ship position and texture
 
 float left;
 float right;
 float top;
 float bottom;
+//creates variables for the ships hitbox
 
 char scoreString [50];
 float score = 0;
 
 float speed;
+//variable for asteroid speed
 
 struct Asteroid{
 float positionX;
@@ -23,12 +27,22 @@ float topHitbox;
 float bottomHitbox;
 int delay;
 };
+//struct storing variables for asteroids
+Texture2D horiAsteroidTexture;
+Texture2D vertAsteroidTexture;
+//A different texture for both horizontal and vertical asteroids.
 #define vertAsteroidCount 3
 #define horiAsteroidCount 3
 struct Asteroid verticalAsteroids [vertAsteroidCount];
 struct Asteroid horizontalAsteroids [horiAsteroidCount];
+//arrays storing vertical and horizontal asteroids
 
+bool gameStarted = false;
 bool gameOver = false;
+//booleans used to control which screen the game shows
+
+Texture2D nonPlayBackground;
+Texture2D playBackground;
 
 void moveShip()
 {
@@ -40,7 +54,8 @@ void moveShip()
     if(shipX<20) shipX = 20;
     if(shipX>580) shipX = 580;
     if(shipY<0) shipY = 0;
-    if(shipY>570) shipY = 570; //prevents ship from going off screen
+    if(shipY>570) shipY = 570; 
+    //prevents ship from going off screen
 }  
 
 void shipHitbox()
@@ -128,15 +143,15 @@ void updateScore()
 void graphics()
 {
     BeginDrawing();
-    ClearBackground(BLACK);
-    DrawTriangle((Vector2){shipX + 20, shipY +15}, (Vector2){shipX, shipY - 15}, (Vector2){shipX - 20, shipY +15}, RAYWHITE); //draws ship
+    DrawTexture(playBackground, 0, 0, WHITE);
+    DrawTexture(shipTexture, shipX, shipY, WHITE); //draws ship
     for(int i =0; i<vertAsteroidCount; i++)
     {
-    DrawCircle(verticalAsteroids[i].positionX, verticalAsteroids[i].positionY, 20, WHITE);
+    DrawTexture(vertAsteroidTexture, verticalAsteroids[i].positionX, verticalAsteroids[i].positionY, WHITE);
     }
     for(int i =0; i<horiAsteroidCount;i++)
     {
-    DrawCircle(horizontalAsteroids[i].positionX, horizontalAsteroids[i].positionY, 20, WHITE);
+    DrawTexture(horiAsteroidTexture, horizontalAsteroids[i].positionX, horizontalAsteroids[i].positionY, WHITE);
     }
     DrawText(scoreString, 10, 10, 20, RAYWHITE);
     EndDrawing();
@@ -145,16 +160,51 @@ void graphics()
 void startScreen()
 {   
     BeginDrawing();
-    ClearBackground(BLACK);
-    DrawText("Press SPACE to start", 300, 300, 100, WHITE);
+    DrawTexture(nonPlayBackground, 0,0,WHITE);
+    DrawText("Press SPACE to start", 80, 240, 40, WHITE);
     EndDrawing();
-
-
 }
+
+void endScreen()
+{
+    BeginDrawing();
+    DrawTexture(nonPlayBackground, 0,0,WHITE);
+    DrawText("GAME O V E R", 150, 240, 40, RED);
+    DrawText("Press R to restart", 100, 340, 40, WHITE);
+    DrawText(scoreString, 10, 10, 20, RAYWHITE);
+    EndDrawing();
+}
+
+void reset()
+{
+    shipX = 300;
+    shipY = 300;
+    score = 0;
+
+    for(int i =0; i<vertAsteroidCount; i++)
+    {
+        verticalAsteroids[i].positionX = GetRandomValue(20,580);
+        verticalAsteroids[i].positionY = 20;
+        verticalAsteroids[i].delay = i*120;
+    }
+
+    for(int i =0; i<horiAsteroidCount; i++)
+    {
+        horizontalAsteroids[i].positionY = GetRandomValue(20,580);
+        horizontalAsteroids[i].positionX = 20;
+        horizontalAsteroids[i].delay = i*120;
+    }
+}
+
 int main(void)
 {   
     InitWindow(600,600, "Asteroid game");
     SetTargetFPS(60);
+    shipTexture = LoadTexture("ship.png");
+    vertAsteroidTexture = LoadTexture("VerticalAsteroid.png");
+    horiAsteroidTexture = LoadTexture("HorizontalAsteroid.png");
+    nonPlayBackground = LoadTexture("baseBackground.png");
+    playBackground = LoadTexture("playingBackground.png");
         for(int i =0; i<vertAsteroidCount; i++)
         {
              verticalAsteroids[i].positionX = GetRandomValue(20,580);
@@ -176,8 +226,27 @@ int main(void)
         }
         speed = 3;
 
-    while (!WindowShouldClose() && !gameOver)
+    while (!WindowShouldClose())
     {   
+        if (!gameStarted)
+        {
+            startScreen();
+            if(IsKeyPressed(KEY_SPACE)){
+                gameStarted = true;
+            }
+            continue;
+        }
+
+        if (gameOver)
+        {
+            endScreen();
+            if(IsKeyPressed(KEY_R))
+            {
+                gameOver = false;
+                reset();
+            }
+            continue;
+        }
         moveShip();
         horizontalAsteroidMovement();
         verticalAsteroidMovement();
@@ -189,9 +258,15 @@ int main(void)
         if (score > 10){
             speed = 5;
         }
-  
-
-        
+        if (score > 30){
+            speed = 7;
+        }
+        if (score > 50){
+            speed = 10;
+        }  
+        if (score >100){
+            speed = 15;
+        }
     }
     CloseWindow();
     return 0;
